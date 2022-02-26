9bb0cc468a654566308955427cc8d5017a199d6e
Kelvin Sherlock <ksherlock@gmail.com>
2022-02-26 18:45:17 -0500

    bump sparkle 2.1.0

--- embedded/download-sparkle.sh
+++ embedded/download-sparkle.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-VERSION=2.0.0
+VERSION=2.1.0
 TAR=Sparkle-${VERSION}.tar.xz
 URL=https://github.com/sparkle-project/Sparkle/releases/download/${VERSION}/Sparkle-${VERSION}.tar.xz
 FRAMEWORK=Sparkle.framework
@@ -16,3 +16,12 @@ if [ ! -e $FRAMEWORK ] ; then tar xfz ../$TAR ; fi
 cd ..
 
 ditto Sparkle-${VERSION}/$FRAMEWORK $FRAMEWORK
+
+# older version of xcode need a Versions/A directory
+
+SW_VERSION=`sw_vers -productVersion`
+case $SW_VERSION in
+  10.14|10.14.*) 
+    if [ ! -e $FRAMEWORK/Versions/A ] ; then ln -s B $FRAMEWORK/Versions/A ; fi
+esac 
+
